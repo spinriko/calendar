e5320260d9451d2a9ca63064bc57a36c6fc4042e
Timothy Little <jakodo@spinriko.com>
2025-12-10 16:53:23 -1000

    WIP: commit local edits before mass-merge (automated save)

--- pto.track/Program.cs
+++ pto.track/Program.cs
@@ -2,6 +2,7 @@ using pto.track.Middleware;
 using pto.track.services;
 using pto.track;
 using Microsoft.Extensions.Hosting.WindowsServices;
+using Microsoft.Extensions.Logging.EventLog;
 
 var options = new WebApplicationOptions
 {
@@ -16,6 +17,24 @@ builder.ConfigureAppConfiguration();
 // centralize service registrations
 builder.ConfigureServices();
 // services are configured in AppServiceExtensions.ConfigureServices
+
+// Ensure host integrates with the Windows Service lifecycle when appropriate.
+// When the process is started by the Service Control Manager, UseWindowsService
+// wires the Generic Host to report start/stop status back to SCM.
+if (OperatingSystem.IsWindows())
+{
+    if (WindowsServiceHelpers.IsWindowsService())
+    {
+        builder.Host.UseWindowsService();
+        // When running as a Windows Service, write important logs to Event Viewer
+        // Use a stable SourceName so Windows admins can find entries easily.
+        builder.Logging.AddEventLog(new EventLogSettings
+        {
+            LogName = "Application",
+            SourceName = "PTO Track"
+        });
+    }
+}
 
 var app = builder.Build();
 
